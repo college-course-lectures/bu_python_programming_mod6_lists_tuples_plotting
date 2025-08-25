c73c9684bc888c8bdb21ade81552691a059bcb76
LaTonya Lewis <developerlewis@gmail.com>
2025-08-25 01:35:16 -0400

    Refactored module2 package to module2_module3 Professor Lewis 8/25/2025

--- src/module1/setup_demo.py
+++ src/module1/setup_demo.py
@@ -1,12 +1,14 @@
-#Simple Python Script:  Greeting Demo Program
+# # #Simple Python Script:  Greeting Demo Program
+#
+# #Ask the user for their name
+# name = input("Enter your name: ")
+#
+# #Ask the user for their favorite programming language
+# language = input("What is your favorite programming language? ")
+#
+# #Display a personalized message
+# print(f"\nHello,  {name}")
+# print(f"It's great that you enjoy {language}.  Keep learning and coding!")
 
-#Ask the user for their name
-name = input("Enter your name: ")
-
-#Ask the user for their favorite programming language
-language = input("What is your favorite programming language? ")
-
-#Display a personalized message
-print(f"\nHello,  {name}")
-print(f"It's great that you enjoy {language}.  Keep learning and coding!")
-
+##Alexander Hunt - August 17th, 2025 - Module 1.3 Assignment
+#The purpose of this code is to calculate the cost of a fiber optic cable purchase.
